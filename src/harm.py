1ddfac6fbda5b64f93b567ba34a6cdda19859cbc
symek <szymon.kapeniak@gmail.com>
2012-10-13 00:20:31 +0200

    Moving signals/slots binding to HarmMainWindowCallbacks() class. (it will be removed to own place at some point.)

--- src/harm.py
+++ src/harm.py
@@ -27,29 +27,6 @@ class HarmMainWindow(QMainWindow, HarmMainWindowGUI):
         self.splash.showMessage(text, Qt.AlignBottom)
         self.app.processEvents()
     
-       
-    def setupSLOTS(self):
-        # Update Job View SIGNAL:
-        #self.connect(self.jobs_view, SIGNAL("clicked(const QModelIndex&)"),  
-        #             self.jobs_view_clicked)
-        #self.connect(self.finished_view, SIGNAL("clicked(const QModelIndex&)"),  
-        #             self.finished_view_clicked)
-        #self.connect(self.tasks_view, SIGNAL("clicked(const QModelIndex&)"),  
-        #             self.tasks_view_clicked)
-        #self.connect(self.right_tab_widget, SIGNAL("currentChanged(const int&)"),  
-        #             self.update_std_views)
-        self.connect(self.refreshAction, SIGNAL('triggered()'), 
-                     self.refreshAll)
-        #self.connect(self.job_view_combo, SIGNAL('currentIndexChanged(int)'), 
-        #             self.change_job_view)
-        #self.connect(self.machine_view_combo, SIGNAL('currentIndexChanged(int)'), 
-        #             self.change_machine_view)
-        #self.connect(self.jobs_filter_line, SIGNAL('textChanged(const QString&)'),\
-        #             self.set_jobs_proxy_model_wildcard)   
-        #self.connect(self.tasks_onlySelected_toggle, SIGNAL('stateChanged(int)'),\
-        #             self.set_tasks_proxy_model_filter)   
-        #self.connect(self.job_details_filter_line, SIGNAL('textChanged(const QString&)'),\
-        #             self.set_job_detail_proxy_model_wildcard)  
     
 
 def main():
