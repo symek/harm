7c1136ca3eeb4aca9e6aa7bd33d033060473f085
symek <szymon.kapeniak@gmail.com>
2012-11-08 17:48:40 +0100

    Changed log script to include all data from qstat

--- scripts/log_sge_db.py
+++ scripts/log_sge_db.py
@@ -2,8 +2,13 @@
 from couchdb import Server
 import sys, os
 sys.path.append("/STUDIO/scripts/harm-sge-db/src")
-from models import JobDetailModel
+from models import JobDetailModel, XmlDictConfig
 from constants import *
+import utilities
+from xml.etree import ElementTree
+
+class Model:
+	pass
 
 db    = 'sge_db'
 jobid = sys.argv[1]
@@ -17,9 +22,16 @@ else:
     db = server.create(db)
 
 
-model = JobDetailModel()
-model.update(SGE_JOB_DETAILS % jobid, 'djob_info')
+#model = JobDetailModel()
+#model.update(SGE_JOB_DETAILS % jobid, 'djob_info')
 #print dict(model._dict)
+tree = ElementTree.parse(os.popen(SGE_JOB_DETAILS % jobid))
+_dict  = XmlDictConfig(tree.getroot())#['djob_info']['element']
+#model = utilities.formatXML(os.popen(SGE_JOB_DETAILS % jobid))
+model = Model()
+model._dict = _dict
+
+#'''
 
 for key in model._dict:
 	if key.startswith("__"):
