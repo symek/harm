3cbe2c2018527160b88733679ea74b9c24ed4d73
symek <szymon.kapeniak@gmail.com>
2012-12-04 17:16:50 +0100

    log_sge_db fixed to handle softimage weirdness.

--- scripts/log_sge_db.py
+++ scripts/log_sge_db.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
-from couchdb import Server
-import sys, os
+import sys
+# Softimage polutes PYTHONPATH, we need to revert sys.path to system defaults:
+# FIXME: Don't hard code it here.
+if True in ['softimage' in path for path in sys.path]:
+    sys.path = [ '/STUDIO/studio-packages', '/usr/lib64/python26.zip',
+                 '/usr/lib64/python2.6', '/usr/lib64/python2.6/plat-linux2', 
+                 '/usr/lib64/python2.6/lib-tk', '/usr/lib64/python2.6/lib-old', 
+                 '/usr/lib64/python2.6/lib-dynload', '/usr/lib64/python2.6/site-packages', 
+                 '/usr/lib64/python2.6/site-packages/gst-0.10', '/usr/lib64/python2.6/site-packages/gtk-2.0', 
+                 '/usr/lib64/python2.6/site-packages/webkit-1.0', '/usr/lib/python2.6/site-packages',
+                 "/STUDIO/scripts/harm-sge-db/src"]
+
+#Now the rest should work as usual:
 sys.path.append("/STUDIO/scripts/harm-sge-db/src")
+import os
+from couchdb import Server
 from constants import *
 import utilities
 from xml.etree import ElementTree
@@ -75,7 +88,7 @@ class XmlDictConfig(dict):
 
 
 class Model:
-	pass
+    pass
 
 db    = 'sge_db'
 jobid = sys.argv[1]
@@ -91,31 +104,32 @@ else:
 
 
 tree = ElementTree.parse(os.popen(SGE_JOB_DETAILS % jobid))
-_dict  = XmlDictConfig(tree.getroot())
+_dict  = XmlDictConfig(tree.getroot())['djob_info']['element']
 
 model = Model()
 model._dict = _dict
+print _dict
 
 #'''
 
 for key in model._dict:
-	if key.startswith("__"):
-		nkey  = key[2:]
-	elif key.startswith("_"):
-		nkey = key[1:]
-	else:
-		continue
-	value = model._dict.pop(key)
-	model._dict[nkey] = value
+    if key.startswith("__"):
+        nkey  = key[2:]
+    elif key.startswith("_"):
+        nkey = key[1:]
+    else:
+        continue
+    value = model._dict.pop(key)
+    model._dict[nkey] = value
 
 
 if not jobid in db:
-	db[jobid] = dict(model._dict)
-	job       = db[jobid]
+    db[jobid] = dict(model._dict)
+    job       = db[jobid]
 else:
-	job       = db[jobid]
-	for key in model._dict:
-		job[key] = model._dict[key]
-	db[jobid] = job
+    job       = db[jobid]
+    for key in model._dict:
+        job[key] = model._dict[key]
+    db[jobid] = job
 
 
