16778d437c80aa238695961092f420f486c56ca8
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-09-29 18:38:59 +0200

    Fixes for Machine view.

--- src/slurm.py
+++ src/slurm.py
@@ -10,6 +10,8 @@ except ImportError:
 SLURM_JOBS_GROUPED_CMD = 'squeue -t <STATES/> -o "%F %P %u %T %j %V %e %M %K %A"'
 SLURM_JOB_DETAILS      = 'scontrol show job <JOBID/>_<TASKID/>'
 SLURM_JOBS_HISTORY_ALL = 'sacct -u <USER/> -o "JobId,Partition,User,State,JobName,Submit,Start,End,ExitCode" -P'
+SLURM_JOB_HISTORY_DETAIL = 'sacct -j <JOBID/> -o "All" -P'
+SLURM_CLUSTER_LIST       = 'scontrol show nodes'
 
 # TODO: Remove and import hafarm utils upon merge.
 def collapse_digits_to_sequence(frames):
@@ -87,8 +89,9 @@ def parse_slurm_output_to_list(output, length=None, reverse_order=True, split_ch
     return lines, head
 
 
-def collapse_list_by_field(data, header, identity_field="ARRAY_JOB_ID",\
-                           collapse_fields=('ARRAY_TASK_ID',), reverse_order=False):
+def collapse_list_by_field(data, header, identity_field="ARRAY_JOB_ID", 
+                          collapse_fields=('ARRAY_TASK_ID',), 
+                          reverse_order=False):
     """ Collapses series of lists based on shared identity_key field,
         and optionally collapses other fields.
     """
@@ -124,12 +127,12 @@ def get_std_output(command):
     """ Performs basic parsing of squeu output.
     """
     out, err = (None, None)
-
+    #TODO: We need proper logging facility. 
     try:
         out, err = subprocess.Popen(command, shell=True, \
         stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
     except: 
-        print "Counld't get Slurm output."
+        print "Counld't get output from: %s" % command # TODO: LOGGER
         print err
     return out, err
 
@@ -177,7 +180,7 @@ def get_current_jobs(max_jobs=300, reverse_order=True):
         return data, header
     return 
 
-def get_accounted_jobs(user, reverse_order=True):
+def get_accounted_jobs(user, jobid=None, reverse_order=True):
     """ Only pending and rendering
     """
     from os import getlogin
@@ -185,6 +188,8 @@ def get_accounted_jobs(user, reverse_order=True):
         user = getlogin()
 
     command   = SLURM_JOBS_HISTORY_ALL.replace("<USER/>", "%s" % user)
+    if jobid:
+        command += " -j %s" % jobid
     data, err = get_std_output(command)
     if data:
         data, head = parse_slurm_output_to_list(data, 500, reverse_order, "|")
@@ -193,6 +198,99 @@ def get_accounted_jobs(user, reverse_order=True):
             header[head.index(item)] = item
         return data, header
     return 
+
+def get_accounted_job_details(jobid=None, reverse_order=True):
+    """ Only pending and rendering
+    """
+
+    command   = SLURM_JOB_HISTORY_DETAIL.replace("<JOBID/>", "%s" % jobid)
+   
+    data, err = get_std_output(command)
+    if data:
+        data, head = parse_slurm_output_to_list(data, 500, reverse_order, "|")
+        header     = OrderedDict()
+        for item in head:
+            header[head.index(item)] = item
+        return data, header
+    return 
+
+
+def get_nodes_info(node=None, reverse_order=False):
+    """ Get details about nodes states from scontrol show node command.
+        Terribly unpleasent due to varying keys and syntax.
+    """
+    def parse_scontrolShowNode_output(output):
+            nodes = output.split("\n\n")
+            data   = []
+            header = []
+            dict_  = OrderedDict()
+            hostname= ""
+            build_header = True
+
+            for node in nodes:
+                preItems = node.split("\n") #node.split()[:-1]
+                items = []
+                for item in preItems:
+                    if len(item.split("=")) > 2:
+                        item = item.split()
+                        items += item
+                    else:
+                        items.append(item)
+                line = []
+                d_   = OrderedDict()
+                for item in items:
+                    var = item.split("=")
+                    name, var = var[0], item[len(var[0])+1:]
+                    if name == 'NodeName':
+                        hostname = var
+                    line += [var]
+                    if build_header:
+                        header += [name.strip()]
+                    d_[name] = var
+
+                data.append(line)
+                dict_[hostname] = d_
+                build_header = False
+
+            return data, header, dict_
+
+    def dict_to_list(_dict):
+        """ Create a list of lists from series of similar dictionaries, 
+            taking care of missing keys in some of them.
+        """
+        header = []
+        data   = []
+        # fill header
+        for nodeid in _dict.keys():
+            node = _dict[nodeid]
+            for head in node.keys():
+                if head not in header:
+                    header.append(head)
+        # gather data
+        for nodeid in _dict.keys():
+            node = _dict[nodeid]
+            line = []
+            for head in header:
+                if head not in node.keys():
+                    line.append("N/A")
+                else:
+                    value = node[head]
+                    line.append(value)
+            data.append(line)
+
+        return data, header
+
+
+    data, err = get_std_output(SLURM_CLUSTER_LIST)
+    if data:
+        data, head, _dict = parse_scontrolShowNode_output(data)
+        data, head = dict_to_list(_dict)
+        header     = OrderedDict()
+        for item in head:
+            header[head.index(item)] = item
+        return data, header
+    return  
+
 
 def get_job_stats(jobid, taskid=""):
     """ As names implies.
