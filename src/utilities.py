3ce893495c2d19fc39d23a0fed68ccd16ff173a8
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-10-12 19:37:55 +0200

    Adding utility to find exec files in a PATH

--- src/utilities.py
+++ src/utilities.py
@@ -336,3 +336,27 @@ def get_rawpixels_from_file(filename, scale_image=1):
     return pixels, w, h
 
 
+# http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python/377028
+def which(program):
+    def is_exe(fpath):
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+    def ext_candidates(fpath):
+        yield fpath
+        for ext in os.environ.get("PATHEXT", "").split(os.pathsep):
+            yield fpath + ext
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            for candidate in ext_candidates(exe_file):
+                if is_exe(candidate):
+                    return candidate
+
+    return None
+
+
